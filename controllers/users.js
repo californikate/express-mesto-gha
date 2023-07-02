abd2c1e4771fc34e4a1ee98379a63943fb0f075a
californikate <regina.plalange8@yandex.ru>
2023-07-02 18:30:29 +0500

    fix: users controllers updateUserAvatar

--- controllers/users.js
+++ controllers/users.js
@@ -61,7 +61,7 @@ const updateUserAvatar = (req, res) => {
   const { avatar } = req.body;
 
   User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
-    .then((user) => ({ data: user }))
+    .then((user) => res.send({ data: user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         res.status(ERROR_CODE.BAD_REQUEST).send({ message: 'Переданы некорректные данные при обновлении аватара' });
